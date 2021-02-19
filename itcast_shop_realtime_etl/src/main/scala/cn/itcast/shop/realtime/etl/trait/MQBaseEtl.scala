96e735538310de3cc1a7a585570d0e8d81ea9749
mrzhang638993 <1446719386@qq.com>
2021-02-19 09:57:53 +0800

    抽取接口，完成相关的方法的实现操作和管理实现

--- itcast_shop_realtime_etl/src/main/scala/cn/itcast/shop/realtime/etl/trait/MQBaseEtl.scala
+++ itcast_shop_realtime_etl/src/main/scala/cn/itcast/shop/realtime/etl/trait/MQBaseEtl.scala
@@ -1,18 +1,27 @@
 package cn.itcast.shop.realtime.etl.`trait`
 
-import  org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
+
+import java.util.Properties
 /**
  * 完成mq中的字符串的编写操作实现
  * */
 /**
  * 消费日志数据，购物车数据，以及评论数据的数据信息
+ * 在kafka中存储的是string类型的数值的。
  * */
 class MQBaseEtl extends BaseEtl[String]{
   /**
    * 读取kafka数据的抽象的方法的信息的
    * */
   override def getKafkaDataStream(topic: String): DataStream[String] ={
-    “”
+     // 创建消费者对象，从kafka中消费数据信息。
+    val props=new Properties()
+    //  配置consumer相关的消费者的对象的信息的。
+    val consumer = new FlinkKafkaConsumer010[String](topic,new SimpleStringSchema(),props)
+    null
   }
 
   /**
---
+++ itcast_shop_realtime_etl/src/main/scala/cn/itcast/shop/realtime/etl/utils/KafkaProps.scala
@@ -0,0 +1,8 @@
+package cn.itcast.shop.realtime.etl.utils
+
+/**
+ * 封装kafka的实现类信息,完成kafka的属性配置操作。
+ * */
+object KafkaProps {
+
+}
